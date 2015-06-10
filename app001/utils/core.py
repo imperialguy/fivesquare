62f1e0dacea3501dcae59259e76f27477346bc19
Ven Karri <karri.ven@gmail.com>
2015-06-10 10:40:14 -0400

    testing Heroku deployment - trial 2

--- app001/utils/core.py
+++ app001/utils/core.py
@@ -1,6 +1,8 @@
+from app001.utils.settings import DB_NAME, DB_HOST, DB_PORT
 from mongoengine import connect
 import logging
 import sys
+import os
 
 
 class Connection(object):
@@ -9,13 +11,15 @@ class Connection(object):
 
     """
 
-    def __init__(self, **kwargs):
+    def __init__(self):
         """Constructor to setup the connection object
 
         """
-        self.connection = connect(kwargs.get('db_name', 'fivesquare'),
-                                  host=kwargs.get('db_host', 'localhost'),
-                                  port=kwargs.get('db_port', 27017))
+        MONGO_URI = os.environ.get('MONGOHQ_URL', None)
+        db_name = 'fivesquare' if not MONGO_URI else MONGO_URI.split("/")[-1]
+        db_host = MONGO_URI if MONGO_URI else 'localhost'
+        db_port = DB_PORT
+        self.connection = connect(db_name, host=db_host, port=db_port)
 
     def __enter__(self):
         """Starts a new Connection by returning the mongoengine connection object
--- app001/utils/settings.py
+++ app001/utils/settings.py
@@ -1,3 +1,8 @@
 # Flask settings
 DEBUG = True
 USE_RELOADER = False
+
+# Database settings
+DB_NAME = 'fivesquare'
+DB_HOST = 'localhost'
+DB_PORT = 27017
